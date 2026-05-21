741cf250f3d45937f57b2d4e7a2780f850f01fcb
jorlyf <jorlyfchannel@gmail.com>
2026-05-21 13:25:52 +0400

    *

--- hosts/honor/default.nix
+++ hosts/honor/default.nix
@@ -620,10 +620,23 @@
 
   programs.git = {
     enable = true;
-    config.user = {
-        name  = "jorlyf";
-        email = "jorlyfchannel@gmail.com";
-    };
+  };
+
+  system.activationScripts.createUserGitconfig = {
+    text = ''
+      touch ${config.users.users.jorlyf.home}/.gitconfig
+
+      cat > ${config.users.users.jorlyf.home}/.gitconfig << EOF
+      [user]
+        name = jorlyf
+        email = jorlyfchannel@gmail.com
+      [core]
+        editor = nano
+      EOF
+
+      chown ${config.users.users.jorlyf.name}:${config.users.users.jorlyf.group} ${config.users.users.jorlyf.home}/.gitconfig
+      chmod 644 ${config.users.users.jorlyf.home}/.gitconfig
+    '';
   };
 
   programs.firefox = {
