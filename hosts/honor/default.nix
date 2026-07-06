fcbdf92e4ca55130c78c088d09acd8ded9a417db
jorlyf <jorlyfchannel@gmail.com>
2026-07-06 16:27:59 +0400

    *

--- hosts/honor/default.nix
+++ hosts/honor/default.nix
@@ -34,15 +34,17 @@
     (flake.outPath + "/user/steam.nix")
   ];
 
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-    };
-    charger = {
-      governor = "powersave";
-      turbo = "never";
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "powersave";
+        turbo = "never";
+      };
     };
   };
 
--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -10,6 +10,7 @@
   imports = [
     (modulesPath + "/hardware/cpu/intel-npu.nix")
     (modulesPath + "/installer/scan/not-detected.nix")
+    (flake.outPath + "/modules/homix.nix")
     (flake.outPath + "/system/services/openssh.nix")
     (flake.outPath + "/system/services/syncthing.nix")
     (flake.outPath + "/system/bluetooth.nix")
@@ -28,9 +29,11 @@
     (flake.outPath + "/user/firefox.nix")
     (flake.outPath + "/user/fish.nix")
     (flake.outPath + "/user/git.nix")
+    (flake.outPath + "/user/gtk.nix")
     (flake.outPath + "/user/obs-studio.nix")
     (flake.outPath + "/user/steam.nix")
   ];
+
   boot.loader.limine = {
     extraEntries = ''
       /Windows
