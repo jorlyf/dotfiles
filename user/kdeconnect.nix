b69638931ca3e670d1a3e1670195320a7526c075
jorlyf <jorlyfchannel@gmail.com>
2026-07-11 01:39:24 +0400

    kdeconnect: systemd autostart

--- user/kdeconnect.nix
+++ user/kdeconnect.nix
@@ -1,8 +1,22 @@
 {
+  config,
   ...
 }:
 {
   programs.kdeconnect = {
     enable = true;
+  };
+
+  systemd.user.services.kdeconnectd = {
+    description = "KDE Connect service";
+    requires = [ "dbus.socket" ];
+    after = [ "dbus.socket" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${config.programs.kdeconnect.package}/libexec/kdeconnectd";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
   };
 }
