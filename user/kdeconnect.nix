7d3f05468580f76508d25e859dbe1139002cdd46
jorlyf <jorlyfchannel@gmail.com>
2026-07-11 15:52:46 +0400

    kdeconnect: fix

--- user/kdeconnect.nix
+++ user/kdeconnect.nix
@@ -7,14 +7,17 @@
     enable = true;
   };
 
-  systemd.user.services.kdeconnectd = {
-    description = "KDE Connect service";
+  systemd.user.services.kdeconnect-indicator = {
+    description = "KDE Connect indicator service";
     requires = [ "dbus.socket" ];
-    after = [ "dbus.socket" ];
+    after = [
+      "dbus.socket"
+      "graphical-session.target"
+    ];
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${config.programs.kdeconnect.package}/libexec/kdeconnectd";
+      ExecStart = "${config.programs.kdeconnect.package}/bin/kdeconnect-indicator";
       Restart = "on-failure";
       RestartSec = 5;
     };
