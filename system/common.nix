50ead7c0cd63e5053a17c578fb4e986f328407b7
jorlyf <jorlyfchannel@gmail.com>
2026-07-11 21:15:19 +0400

    firefox: nvidia media аппаратное декодирование

--- system/common.nix
+++ system/common.nix
@@ -4,8 +4,11 @@
 {
   time.timeZone = "Europe/Samara";
   system.stateVersion = "26.05";
-  environment.etc.machine-id.text = "15e857fea7a440f9b8d7bc7b0c4a584b";
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
   zramSwap.enable = true;
+  environment = {
+    etc.machine-id.text = "15e857fea7a440f9b8d7bc7b0c4a584b";
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+  };
 }
--- user/firefox.nix
+++ user/firefox.nix
@@ -4,5 +4,17 @@
 {
   programs.firefox = {
     enable = true;
+    preferences = {
+      "media.ffmpeg.vaapi.enabled" = true;
+      "media.hardware-video-decoding.force-enabled" = true;
+      "media.rdd-ffmpeg.enabled" = true;
+      "gfx.x11-egl.force-enabled" = true;
+      "widget.dmabuf.force-enabled" = true;
+      "media.av1.enabled" = true;
+    };
+  };
+
+  environment.sessionVariables = {
+    MOZ_DISABLE_RDD_SANDBOX = "1";
   };
 }
