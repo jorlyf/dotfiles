d59fc565181f58f34032c3d7ae0073ddaa7036f6
jorlyf <jorlyfchannel@gmail.com>
2026-07-11 20:22:51 +0400

    *

--- user/xdg.nix
+++ user/xdg.nix
@@ -20,4 +20,11 @@
       enable = true;
     };
   };
+
+  environment.sessionVariables = {
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
 }
